02db17b3c1d6a9daa2f5e0a34f53adc369ab0fdb
Harlongbi-Phangcho <harbisonphangcho1999@gmail.com>
2026-06-12 13:08:08 +0530

    refactor Button component for improved readability and styling consistency

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Button({
-    children,
-    className = "",
-    type = "button",
-    ...props
+  children,
+  type = "button",
+  className = "",
+  ...props
 }) {
   return (
-    <button type={type} className={`btn ${className}`} {...props}>
-        {children}
+    <button
+      type={type}
+      className={`
+        px-5
+        py-2
+        rounded-full
+        bg-red-600
+        hover:bg-red-700
+        text-white
+        font-medium
+        transition
+        duration-200
+        cursor-pointer
+        ${className}
+      `}
+      {...props}
+    >
+      {children}
     </button>
-  )
+  );
 }
 
-export default Button
+export default Button;
