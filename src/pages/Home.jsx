0f2b83af252390392a926122ed9b5b48ea8b19f7
Harlongbi-Phangcho <harbisonphangcho1999@gmail.com>
2026-06-15 23:05:39 +0530

    add date-fns dependency and implement video fetching in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,42 @@
-import React from "react";
-import Card from "../components/Card"
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import { api } from "../api/axios";
+import { FaSpinner } from "react-icons/fa";
+
 
 function Home() {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const getAllVideos = async () => {
+      try {
+        const res = await api.get("/videos");
+        setVideos(res.data.data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getAllVideos();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-zinc-950 flex items-center justify-center">
+        <FaSpinner className="text-red-500 text-5xl animate-spin" />
+        <p className="text-zinc-400">Loading...</p>
+      </div>
+    );
+  }
+
+  console.log(videos)
   return (
     <div className="bg-zinc-950 min-h-screen text-white">
-
       {/* Hero Section */}
       <section className="px-6 py-14 text-center">
-
-        <h1 className="text-5xl font-bold">
-          Watch. Upload. Share.
-        </h1>
+        <h1 className="text-5xl font-bold">Watch. Upload. Share.</h1>
 
         <p className="text-zinc-400 mt-4 text-lg">
           Discover videos from creators around the world
@@ -24,7 +50,6 @@ function Home() {
       {/* Categories */}
       <section className="px-8 mb-8">
         <div className="flex gap-3 overflow-x-auto">
-
           {[
             "All",
             "Music",
@@ -46,35 +71,20 @@ function Home() {
 
       {/* Video Grid */}
       <section className="px-6 pb-10">
-
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-
-          <Card
-            thumbnail="https://images.unsplash.com/photo-1498050108023-c5249f4df085"
-            avatar="https://i.pravatar.cc/100"
-            title="Build Full Stack YouTube Clone"
-            channelName="Code World"
-            views="20K"
-            uploadedAt="2 days ago"
-          />
-
-          <Card
-            thumbnail="https://images.unsplash.com/photo-1516321318423-f06f85e504b3"
-            avatar="https://i.pravatar.cc/101"
-            title="React Hook Form Complete Guide"
-            channelName="Frontend Master"
-            views="15K"
-            uploadedAt="5 days ago"
-          />
-
-          <Card
-            thumbnail="https://images.unsplash.com/photo-1516116216624-53e697fedbea"
-            avatar="https://i.pravatar.cc/102"
-            title="Node.js Authentication Tutorial"
-            channelName="Backend Pro"
-            views="30K"
-            uploadedAt="1 week ago"
-          />
+          {videos.map((video) => (
+            <Card
+              key={video._id}
+              id={video._id}
+              thumbnail={video.thumbnail}
+              avatar={video.ownerDetails.avatar}
+              title={video.title}
+              channelName={video.ownerDetails.username}
+              views={video.views}
+              uploadedAt={video.updatedAt}
+            />
+          ))}
+          
         </div>
       </section>
     </div>
